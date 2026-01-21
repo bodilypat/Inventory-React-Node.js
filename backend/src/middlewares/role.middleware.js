e10e3a9e51a6a355e09ad4b66e8d8ff71516b11d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-01-21 15:15:51 -0800

    Add files via upload

---
+++ backend/src/middlewares/role.middleware.js
@@ -0,0 +1,9 @@
+//src/middlewares/role.middleware.js
+
+Medule.exports = (role) => (req, res, next) => {
+    if (req.user.role !== role) {
+        return res.status(403).json({ message: 'Access denied. Insufficient permissions.' });
+    }
+    next();
+}
+
